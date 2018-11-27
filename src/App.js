1b161b0fff399e1ad776445ca97a8c23b1ffe9a1
jeremyhartman <jeremy.fine.hartman@gmail.com>
2018-11-27 15:58:38 -0500

    added conditional to detect space bar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
-    
+
     this.state = {currentKey: ''}
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
@@ -14,12 +14,15 @@ class App extends Component {
     let s = this.state.currentKey
     console.log(`s: ${s}`)
     this.setState({currentKey: s+String.fromCharCode(e.keyCode)});
+    if(e.keycode == 32){
+      console.log('you hit space')
+    }
   }
 
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyPress);
   }
-  
+
   componentWillUnmount() {
     document.removeEventListener('keydown', this.handleKeyPress);
   }
