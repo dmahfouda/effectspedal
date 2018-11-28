c1c42002aa2d336c4b9bd77d25b797bc6a03cebf
dmahfouda <david.mahfouda@gmail.com>
2018-11-28 15:59:56 -0500

    now returning thesaurus requests

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express()
 const port = 3000
 
 app.get('/antonym', (req, res) => {
-	console.log(`req: ${req.word}`)
-	res.send(tcom.search(req.word))
+	console.log(`req: ${req.query.word}`)
+	res.send(tcom.search(req.query.word))
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
