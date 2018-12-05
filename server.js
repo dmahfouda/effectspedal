354b9439183ad56c4bc8fe2b3357efa29bef6ad1
dmahfouda <david.mahfouda@gmail.com>
2018-12-05 16:31:31 -0500

    making queries to axios and erroring on render

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const tcom = require('thesaurus-com')
 const app = express()
+const cors = require('cors')
 const port = 3001
+
+app.use(cors())
 
 app.get('/antonym', (req, res) => {
 	console.log(`req: ${req.query.word}`)
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props){
-    super(props);
+    super(props)
 
     this.state = {
       currentKeyBuffer: '',
@@ -14,56 +15,39 @@ class App extends Component {
 
   handleKeyPress = (e) => {
     let s = this.state.currentKeyBuffer
-    console.log(`s: ${s}`)
     this.setState({currentKeyBuffer: s+String.fromCharCode(e.keyCode)});
     if (e.keyCode == 32) {
-      this.setState({
-        currentKeyBuffer: '',
-        wordsArray: [
-          ...this.state.wordsArray, 
-          {
-            word: this.state.currentKeyBuffer, 
-            decisionSpace: ['thing']
-          }
-        ]
+      console.log(`this.state.currentKeyBuffer ${this.state.currentKeyBuffer}`)
+      axios.get(`http://localhost:3001/antonym`,{params:{word:this.state.currentKeyBuffer}})
+        .then(res => {
+          const words = res.data;
+          console.log(words)
+          // this.setState({ persons });
+        this.setState({
+          currentKeyBuffer: '',
+          wordsArray: [
+            ...this.state.wordsArray, 
+            {
+              word: this.state.currentKeyBuffer, 
+              decisionSpace: res.data.antonyms
+            }
+          ]
+        })
       })
-      this.setCSSWidth('.word-container')
-      console.log(this.state.wordsArray)
     }
-  }
-
-  getWidth = (selector) => {
-    let activeBlock = document.querySelector(selector)
-    return activeBlock.getBoundingClientRect().width
-  }
-
-  setCSSWidth = (selector) => {
-    let wordContainerBlocks = document.querySelectorAll(selector)
-      wordContainerBlocks.forEach((block)=>{
-        block.style.width = this.getWidth(".active")+'px'
-      })
   }
 
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyPress);
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyPress);
-  }
-
-  // alternateWord = () => {
-  //   setInterval(()=>{
-
-  //   }, 1000)
-  // }
-
   renderWords = () => {
-    return this.state.wordsArray.map(({word,decisionSpace})=>{
+    this.state.wordsArray.forEach((wordObject) => { 
       return(
         <div className='word-container'>
-          <div className='active'>{word}</div>
-          {decisionSpace.map((word,index)=>(<div className='passive' key={index}>{word}</div>))}
+          /*<div className='active'>{word}</div>
+          {decisionSpace.map((word,index)=>(<div className='passive' key={index}>{word}</div>))}*/
+          <div className='active'>{wordObject.decisionSpace[Math.floor(Math.random()*wordObject.decisionSpace.length)]}</div>
         </div>
       )
     })
@@ -80,4 +64,3 @@ class App extends Component {
 }
 
 export default App;
-// {this.state.wordsArray.join(' ')}
